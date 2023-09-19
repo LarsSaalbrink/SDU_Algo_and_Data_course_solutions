8ce7c17444ed2f6a8d3e0d6b740505def0efa8a5
Lars Saalbrink <larssaalbrink@gmail.com>
2023-09-19 09:08:26 +0200

    Nicer looking feedback

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 # Get subfolder name from first argument if available, else error
 if [ -z "$1" ]
   then
-    echo "!!!No folder supplied!!!"
+    echo -e "\n!!!No folder supplied!!!\n"
     exit 1
 fi
 SUBFOLDER=$1
@@ -9,30 +9,30 @@ SUBFOLDER=$1
 # if second argument is "clean", then delete "out" folder
 if [ "$2" = "clean" ]
   then
-    echo "--------------------Deleting build folder--------------------"
+    echo -e "--------------------Deleting build folder--------------------"
     rm -rf $SUBFOLDER/out
-    echo "Done deleteing build folder!"
+    echo -e "\nDone deleteing build folder!\n"
 fi
 
 # If subfolder has no folder "out", create fresh build system with "cmake -S . -B out/"
 if [ ! -d "$SUBFOLDER/out" ] 
   then
-    echo "--------------------Creating build folder--------------------"
+    echo -e "--------------------Creating build folder--------------------"
     cmake -S $SUBFOLDER -B $SUBFOLDER/out
-    echo "Done creating buildfolder!"
+    echo -e "\nDone creating buildfolder!\n"
 fi
 
 # Build with command "make -C out/"
-echo "--------------------Building--------------------"
+echo -e "--------------------Building--------------------"
 make -C $SUBFOLDER/out
-echo "Done building!"
+echo -e "\nDone building!\n"
 
 # Run with command "./out/main"
-echo "--------------------Running--------------------"
+echo -e "--------------------Running--------------------"
 $SUBFOLDER/out/main
-echo "Done running code!"
+echo -e "\nDone running code!\n"
 
 # When done running, call "gprof out/main > analysis.txt"
-echo "--------------------Updating profiling analysis--------------------"
+echo -e "--------------------Updating profiling analysis--------------------"
 gprof $SUBFOLDER/out/main > $SUBFOLDER/analysis.txt
-echo "All done!"
+echo -e "\nAll done!\n"
